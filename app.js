959e57beac8f7a3ed2328dadc78bb2a061c72140
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-10 11:01:36 -0500

    terminando funcion de eliminar tarea

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const taskActives = document.getElementById('actives')
 const checkTask = document.getElementById('check')
 
 let LENGTH_TO_COMPLETE = 0
+localStorage.getItem('id') === null
+? localStorage.setItem('id', 0) 
+: console.log('')
 
 const saveLocalStorage = (key, value) => {
     localStorage.setItem(key, value)
@@ -13,26 +16,26 @@ const saveLocalStorage = (key, value) => {
 
 const addTask = (task) => {
     const allTask = localStorage.getItem('all') !== null
-    ? JSON.parse( localStorage.getItem('all') )
-    : []
+        ? JSON.parse(localStorage.getItem('all'))
+        : []
     const arrCopy = [...allTask, task]
-    saveLocalStorage('all', JSON.stringify( arrCopy ) )
+    saveLocalStorage('all', JSON.stringify(arrCopy))
 }
 
 const filterTaskCompleted = (tasks) => {
-    const filtered = tasks.filter(task => task.isCompleted )
+    const filtered = tasks.filter(task => task.isCompleted)
     return filtered.length > 0 ? filtered : []
 }
 
 const filterTaskActives = (tasks) => {
-    const filtered = tasks.filter(task => task.isCompleted === false )
+    const filtered = tasks.filter(task => task.isCompleted === false)
     return filtered.length > 0 ? filtered : []
 }
 
 const checkedTask = e => {
 
-    if (e.target.idTask === e.target.idtwoTask ) {
-        
+    if (e.target.dataset.idTask === e.target.dataset.idtwoTask) {
+
         const el = document.querySelector(`[check="${e.target.dataset.idTask}"]`)
         el.classList.toggle('check__task--checked')
 
@@ -40,37 +43,56 @@ const checkedTask = e => {
 
 }
 
+const deleteTask = e => {
+
+    if (e.target.dataset.btnTask === e.target.dataset.btntwoTask) {
+
+        console.log('btn', e.target.dataset)
+
+        const idTask = Number(e.target.dataset.btnTask)
+        const taskArrayNew = [...JSON.parse(localStorage.getItem('all') ) ].filter(e => e.id !== idTask) 
+        console.log(idTask)
+        console.log(taskArrayNew)
+        saveLocalStorage('all', JSON.stringify(taskArrayNew)  )
+        printTask( JSON.parse(localStorage.getItem('all') ) )
+
+    }
+
+}
+
 const printTask = (task) => {
 
-    const taskContent = document.getElementById('task')
+    const taskContent = document.getElementById('tasks')
     const fragment = document.createDocumentFragment();
     taskContent.innerHTML = ''
-    
+
     if (task !== null) {
-        
+
         task.forEach((taskElement, i) => {
-            console.log(i)
+
             const div = document.createElement('div')
             const divChek = document.createElement('div')
             const h2 = document.createElement('h2')
             const button = document.createElement('button')
 
             divChek.setAttribute('id', 'check')
-            divChek.setAttribute('class','check__task')
+            divChek.setAttribute('class', 'check__task')
             divChek.addEventListener('click', checkedTask)
-            divChek.setAttribute('data-id-task',`${i}`)
-            divChek.setAttribute('data-idTwo-task',`${i}`)
-            divChek.setAttribute('check',`${i}`)
+            divChek.setAttribute('data-id-task', `${taskElement.id}`)
+            divChek.setAttribute('data-idTwo-task', `${taskElement.id}`)
+            divChek.setAttribute('check', `${taskElement.id}`)
 
-            button.setAttribute('data-btn-task',`${i}`)
-            button.setAttribute('class','task__delete_btn')
+            button.setAttribute('data-btn-task', `${taskElement.id}`)
+            button.setAttribute('data-btnTwo-task', `${taskElement.id}`)
+            button.setAttribute('class', 'task__delete_btn')
+            button.addEventListener('click', deleteTask)
             button.textContent = 'X'
-            
-            h2.setAttribute('class','title__task')
+
+            h2.setAttribute('class', 'title__task')
             h2.textContent = taskElement.title
-            
-            
-            div.setAttribute('class','task')
+
+
+            div.setAttribute('class', 'task')
             div.appendChild(divChek)
             div.appendChild(h2)
             div.appendChild(button)
@@ -82,15 +104,15 @@ const printTask = (task) => {
         taskContent.appendChild(fragment)
     }
 
-    if (task.length === 0 && task !== null ) {
+    if (task.length === 0 && task !== null) {
         const h2 = document.createElement('h2')
         h2.textContent = 'Ninguna Tarea'
 
         taskContent.innerHTML = ''
         taskContent.appendChild(h2)
-        
+
     }
-    
+
 }
 
 addInput.addEventListener('keyup', e => {
@@ -100,10 +122,18 @@ addInput.addEventListener('keyup', e => {
 
     if (KEY_CODE === 'Enter' && titleTask !== '') {
 
+        let id = localStorage.getItem('all') === null
+            ? 0
+            : Number(localStorage.getItem('id')) + 1
+
+
         const task = {
             title: titleTask,
-            isCompleted: false
+            isCompleted: false,
+            id
         }
+
+        localStorage.setItem('id', id)
 
         addTask(task)
         printTask(JSON.parse(localStorage.getItem('all')))
@@ -117,49 +147,60 @@ addInput.addEventListener('keyup', e => {
 
 addBtn.addEventListener('click', e => {
 
+
     const input = document.getElementById('add-input')
     const titleTask = input.value;
 
+    let id = localStorage.getItem('all') === null
+        ? 0
+        : Number(localStorage.getItem('id')) + 1
+
+
     const task = {
         title: titleTask,
-        isCompleted: false
+        isCompleted: false,
+        id
     }
+
+    localStorage.setItem('id', id)
 
     addTask(task)
     printTask(JSON.parse(localStorage.getItem('all')))
 
     input.value = ''
 
+
+
 })
 
 completedTask.addEventListener('click', e => {
 
-    const allTask = JSON.parse( localStorage.getItem('all') )
+    const allTask = JSON.parse(localStorage.getItem('all'))
     const completedTask = filterTaskCompleted(allTask)
 
-    saveLocalStorage('completed', JSON.stringify(completedTask) )
-    const completedTaskFilter = JSON.parse( localStorage.getItem('completed') )
-    printTask( completedTaskFilter )
+    saveLocalStorage('completed', JSON.stringify(completedTask))
+    const completedTaskFilter = JSON.parse(localStorage.getItem('completed'))
+    printTask(completedTaskFilter)
 
 })
 
-allTask.addEventListener('click', e  => {
-    const data = JSON.parse(localStorage.getItem('all')) 
+allTask.addEventListener('click', e => {
+    const data = JSON.parse(localStorage.getItem('all'))
     printTask(data)
 })
 
 taskActives.addEventListener('click', e => {
 
-    const allTask = JSON.parse( localStorage.getItem('all') )
+    const allTask = JSON.parse(localStorage.getItem('all'))
     const activesTask = filterTaskActives(allTask)
 
-    saveLocalStorage('actives', JSON.stringify(activesTask) )
-    const activesTaskFilter = JSON.parse( localStorage.getItem('actives') )
-    printTask(activesTaskFilter )
+    saveLocalStorage('actives', JSON.stringify(activesTask))
+    const activesTaskFilter = JSON.parse(localStorage.getItem('actives'))
+    printTask(activesTaskFilter)
 
 })
 
 const data = localStorage.getItem('all') !== null
-? JSON.parse(localStorage.getItem('all'))
-: [] 
+    ? JSON.parse(localStorage.getItem('all'))
+    : []
 printTask(data)
