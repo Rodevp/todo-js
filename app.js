fd43023ea7ea83817a85d1d802c01bfdac092176
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-10 09:10:28 -0500

    completando filtros y arreglando bug de filter

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const addInput = document.getElementById('add-input')
 const addBtn = document.getElementById('add_btn')
-const completedTask = document.getElementById('actives')
+const completedTask = document.getElementById('completed')
+const allTask = document.getElementById('all_btn')
+const taskActives = document.getElementById('actives')
+
 let LENGTH_TO_COMPLETE = 0
 
 const saveLocalStorage = (key, value) => {
@@ -15,13 +18,14 @@ const addTask = (task) => {
     saveLocalStorage('all', JSON.stringify( arrCopy ) )
 }
 
-const filterTask = (e) => {
-    const filtered = JSON.parse( localStorage.getItem('all') )
-    console.log(filtered)
+const filterTaskCompleted = (tasks) => {
+    const filtered = tasks.filter(task => task.isCompleted )
+    return filtered.length > 0 ? filtered : []
+}
 
-    const filterData = filtered.filter(task => task.isCompleted !== false)
-    console.log(filterData)
-
+const filterTaskActives = (tasks) => {
+    const filtered = tasks.filter(task => task.isCompleted === false )
+    return filtered.length > 0 ? filtered : []
 }
 
 const checkTask = e => {
@@ -72,6 +76,15 @@ const printTask = (task) => {
 
         taskContent.appendChild(fragment)
     }
+
+    if (task.length === 0 && task !== null ) {
+        const h2 = document.createElement('h2')
+        h2.textContent = 'Ninguna completada'
+
+        taskContent.innerHTML = ''
+        taskContent.appendChild(h2)
+        
+    }
     
 }
 
@@ -114,7 +127,32 @@ addBtn.addEventListener('click', e => {
 
 })
 
-completedTask.addEventListener('click', filterTask)
+completedTask.addEventListener('click', e => {
+
+    const allTask = JSON.parse( localStorage.getItem('all') )
+    const completedTask = filterTaskCompleted(allTask)
+
+    saveLocalStorage('completed', JSON.stringify(completedTask) )
+    const completedTaskFilter = JSON.parse( localStorage.getItem('completed') )
+    printTask( completedTaskFilter )
+
+})
+
+allTask.addEventListener('click', e  => {
+    const data = JSON.parse(localStorage.getItem('all')) 
+    printTask(data)
+})
+
+taskActives.addEventListener('click', e => {
+
+    const allTask = JSON.parse( localStorage.getItem('all') )
+    const activesTask = filterTaskActives(allTask)
+
+    saveLocalStorage('actives', JSON.stringify(activesTask) )
+    const activesTaskFilter = JSON.parse( localStorage.getItem('actives') )
+    printTask(activesTaskFilter )
+
+})
 
 const data = JSON.parse(localStorage.getItem('all')) 
 printTask(data)
